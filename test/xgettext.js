c4b99a70ce419dfcc37881b5a60c9f593029993c
Andrew Duthie <andrew@andrewduthie.com>
2016-10-31 12:48:44 -0400

    Update code style to adhere to ESLint

--- test/xgettext.js
+++ test/xgettext.js
@@ -4,100 +4,100 @@ var expect = require( 'chai' ).expect,
 it( 'should be instantiable', function() {
 	var parser = new XGettext();
 	expect( parser ).to.be.instanceof( XGettext );
-});
+} );
 
 it( 'should return array of translatable strings', function() {
 	var source = '_( "Hello World!" );',
 		matches = new XGettext().getMatches( source );
 
-	expect( matches ).to.deep.equal( [{ string: 'Hello World!', line: 1, column: 0 }] );
-});
+	expect( matches ).to.deep.equal( [ { string: 'Hello World!', line: 1, column: 0 } ] );
+} );
 
 it( 'should return array of translatable strings, including comment on same line', function() {
 	var source = '_( "Hello World!" ); /* translators: greeting */',
 		matches = new XGettext().getMatches( source );
 
-	expect( matches ).to.deep.equal( [{ string: 'Hello World!', comment: 'greeting', line: 1, column: 0 }] );
-});
+	expect( matches ).to.deep.equal( [ { string: 'Hello World!', comment: 'greeting', line: 1, column: 0 } ] );
+} );
 
 it( 'should return array of translatable strings, including comment on previous line', function() {
 	var source = '/* translators: greeting */\n_( "Hello World!" );',
 		matches = new XGettext().getMatches( source );
 
-	expect( matches ).to.deep.equal( [{ string: 'Hello World!', comment: 'greeting', line: 2, column: 0 }] );
-});
+	expect( matches ).to.deep.equal( [ { string: 'Hello World!', comment: 'greeting', line: 2, column: 0 } ] );
+} );
 
 it( 'should enable developer to provide custom keyword logic returning a string', function() {
 	var source = '_x( "Hello World!", "greeting" );',
-		parser = new XGettext({
+		parser = new XGettext( {
 			keywords: {
-				'_x': function( match ) {
+				_x: function( match ) {
 					if ( 2 === match.arguments.length ) {
-						return match.arguments[1].value + '\u0004' + match.arguments[0].value;
-					} else {
-						return match.arguments[0].value;
+						return match.arguments[ 1 ].value + '\u0004' + match.arguments[ 0 ].value;
 					}
+
+					return match.arguments[ 0 ].value;
 				}
 			}
-		}),
+		} ),
 		matches = parser.getMatches( source );
 
-	expect( matches ).to.deep.equal( [{ string: 'greeting\u0004Hello World!', line: 1, column: 0 }] );
-});
+	expect( matches ).to.deep.equal( [ { string: 'greeting\u0004Hello World!', line: 1, column: 0 } ] );
+} );
 
 it( 'should enable developer to provide custom keyword logic returning an object', function() {
 	var source = '_( "Hello World!" );',
-		parser = new XGettext({
+		parser = new XGettext( {
 			keywords: {
-				'_': function( match ) {
+				_: function() {
 					return { isOkay: true };
 				}
 			}
-		}),
+		} ),
 		matches = parser.getMatches( source );
 
-	expect( matches ).to.deep.equal( [{ isOkay: true }] );
-});
+	expect( matches ).to.deep.equal( [ { isOkay: true } ] );
+} );
 
 it( 'should enable developer to provide custom translator comment prefix', function() {
 	var source = '_( "Hello World!" ); /* note: greeting */',
-		parser = new XGettext({
+		parser = new XGettext( {
 			commentPrefix: 'note:'
-		}),
+		} ),
 		matches = parser.getMatches( source );
 
-	expect( matches ).to.deep.equal( [{ string: 'Hello World!', comment: 'greeting', line: 1, column: 0 }] );
-});
+	expect( matches ).to.deep.equal( [ { string: 'Hello World!', comment: 'greeting', line: 1, column: 0 } ] );
+} );
 
 it( 'should accept a number as keyword value to represent argument position', function() {
-  var source = '_( null, "Hello World!" );',
-    parser = new XGettext({
-      keywords: {
-        '_': 2
-      }
-    }),
-    matches = parser.getMatches( source );
+	var source = '_( null, "Hello World!" );',
+		parser = new XGettext( {
+			keywords: {
+				_: 2
+			}
+		} ),
+		matches = parser.getMatches( source );
 
-  expect( matches ).to.deep.equal( [{ string: 'Hello World!', line: 1, column: 0 }] );
-});
+	expect( matches ).to.deep.equal( [ { string: 'Hello World!', line: 1, column: 0 } ] );
+} );
 
 it( 'should match functions that are the last element of a sequence (comma) expression', function() {
 	var source = '(0, transpilerGeneratedName._)("Hello World!")',
 		matches = new XGettext().getMatches( source );
 
-	expect( matches ).to.deep.equal( [{ 'string': 'Hello World!', line: 1, column: 0 }] );
-});
+	expect( matches ).to.deep.equal( [ { string: 'Hello World!', line: 1, column: 0 } ] );
+} );
 
 it( 'should match functions that are the last element of a recursive sequence (comma) expression', function() {
 	var source = '(0, (0, transpilerGeneratedName._))("Hello World!")',
 		matches = new XGettext().getMatches( source );
 
-	expect( matches ).to.deep.equal( [{ 'string': 'Hello World!', line: 1, column: 0 }] );
-});
+	expect( matches ).to.deep.equal( [ { string: 'Hello World!', line: 1, column: 0 } ] );
+} );
 
 it( 'should parse ecma6 by default', function() {
 	var source = 'const i = 0; _("Hello World!");',
 		matches = new XGettext().getMatches( source );
 
-	expect( matches ).to.deep.equal( [{ 'string': 'Hello World!', line: 1, column: 13 }] );
-});
+	expect( matches ).to.deep.equal( [ { string: 'Hello World!', line: 1, column: 13 } ] );
+} );
--- xgettext.js
+++ xgettext.js
@@ -1,16 +1,21 @@
 var _ = require( 'lodash' ),
 	parser = require( 'babylon' ),
 	walk = require( 'estree-walker' ).walk;
+
 /**
- * XGettext will parse a given input string for any instances of i18n function calls,
- * returning an array of objects for all translatable strings discovered.
+ * XGettext will parse a given input string for any instances of i18n function
+ * calls, returning an array of objects for all translatable strings
+ * discovered.
  *
- * @param  {Object} Options to use when parsing the input. Refer to XGettext.defaultOptions
- * for available options and a description for each
- * @return {Array} An array of objects for all translatable strings discovered.
+ * @param {Object} options Options to use when p.arsing the input. Refer to
+ *                         XGettext.defaultOptions for available options and a
+ *                         description for each
  */
 var XGettext = module.exports = function( options ) {
-	if ( 'object' !== typeof options ) options = {};
+	if ( 'object' !== typeof options ) {
+		options = {};
+	}
+
 	this.options = _.extend( {}, XGettext.defaultOptions, options );
 	this.options.keywords = this._normalizeKeywords( this.options.keywords );
 	this.options.keywordFunctions = Object.keys( this.options.keywords );
@@ -19,24 +24,28 @@ var XGettext = module.exports = function( options ) {
 
 XGettext.defaultOptions = {
 	/**
-	 * A key-value pair of keyword function names to be mapped into their desired string value.
-	 * Transform functions are passed a match including three keys: `keyword` (the matched keyword),
-	 * `arguments` (a CallExpression arguments array), and `comment` if one exists. It is expected
-	 * that this function will return a string or an array of strings. Alternatively, define value as
-	 * number to return value in that argument position on a 1-based index.
+	 * A key-value pair of keyword function names to be mapped into their
+	 * desired string value. Transform functions are passed a match including
+	 * three keys: `keyword` (the matched keyword), `arguments` (a
+	 * CallExpression arguments array), and `comment` if one exists. It is
+	 * expected that this function will return a string or an array of strings.
+	 * Alternatively, define value as number to return value in that argument
+	 * position on a 1-based index.
 	 *
 	 * @type {Object}
 	 * @see https://github.com/babel/babylon/blob/master/ast/spec.md
 	 */
 	keywords: {
-		'_': 1
+		_: 1
 	},
 
 	/**
-	 * Optionally match translator comments to be included with translatable strings.
+	 * Optionally match translator comments to be included with translatable
+	 * strings.
 	 *
-	 * If undesired, set as `undefined`. A comment will be matched if it is prefixed by this option
-	 * and occurs either on the same or previous line as the matched keyword.
+	 * If undesired, set as `undefined`. A comment will be matched if it is
+	 * prefixed by this option and occurs either on the same or previous line
+	 * as the matched keyword.
 	 *
 	 * @type {String,undefined}
 	 */
@@ -52,14 +61,17 @@ XGettext.defaultOptions = {
 };
 
 /**
- * Returns an array of objects for all strings matched by the keywords defined in the `keyword`
- * option property.
+ * Returns an array of objects for all strings matched by the keywords defined
+ * in the `keyword` option property.
  *
- * Each object in the array contains a `string` key where the value is determined by the
- * corresponding keyword mapping function. An object may also contain a `comment` key if the
- * `commentPrefix` option is provided and a comment is associated with the matched keyword.
+ * Each object in the array contains a `string` key where the value is
+ * determined by the corresponding keyword mapping function. An object may also
+ * contain a `comment` key if the `commentPrefix` option is provided and a
+ * comment is associated with the matched keyword.
  *
- * @return {Array} An array containing objects for each matched occurrance of a keyword function
+ * @param  {String} input String from which to find matches
+ * @return {Array}        An array containing objects for each matched
+ *                        occurrance of a keyword function
  */
 XGettext.prototype.getMatches = function( input ) {
 	var parsedInput, matches, transformedMatches;
@@ -71,21 +83,24 @@ XGettext.prototype.getMatches = function( input ) {
 	matches = this._discoverMatches( parsedInput );
 
 	// Use configured keyword transforms to parse string value
-	transformedMatches = _( matches ).map(function(match) {
+	transformedMatches = _( matches ).map( function( match ) {
 		return this._transformMatch( match );
-	}.bind(this) ).flatten().value();
+	}.bind( this ) ).flatten().value();
 
 	return transformedMatches;
-}
+};
 
 /**
- * Returns an object containing keyword functions where number values are replaced with a function
- * returning the nth argument on a 1-based index
+ * Returns an object containing keyword functions where number values are
+ * replaced with a function returning the nth argument on a 1-based index.
  *
  * @private
- * @return {Object} An object containing keyword functions where number values are replaced with a
- * function returning the nth argument on a 1-based index
  * @see https://www.gnu.org/software/gettext/manual/html_node/xgettext-Invocation.html
+ *
+ * @param  {Object} keywords Original keywords object configuration
+ * @return {Object}          An object containing keyword functions where
+ *                           number values are replaced with a function
+ *                           returning the nth argument on a 1-based index
  */
 XGettext.prototype._normalizeKeywords = function( keywords ) {
 	var normalizedKeywords = {};
@@ -98,15 +113,16 @@ XGettext.prototype._normalizeKeywords = function( keywords ) {
 };
 
 /**
- * If passed a number, returns a function which returns the nth argument on a 1-based index.
- * Otherwise, returns the passed argument.
+ * If passed a number, returns a function which returns the nth argument on a
+ * 1-based index. Otherwise, returns the passed argument.
  *
- * @param  {Number,Function} keyword A number or function to be normalized
- * @return {Function} A function to be used in place of the passed argument
+ * @param  {(Number|Function)} keyword A number or function to be normalized
+ * @return {Function}                  A function to be used in place of the
+ *                                     passed argument
  */
 XGettext.prototype._normalizeKeyword = function( keyword ) {
 	if ( 'number' === typeof keyword ) {
-		return (function( argnum ) {
+		return ( function( argnum ) {
 			var argumentPosition = argnum - 1;
 
 			return function( match ) {
@@ -115,19 +131,21 @@ XGettext.prototype._normalizeKeyword = function( keyword ) {
 					return match.arguments[ argumentPosition ].value;
 				}
 			};
-		}( keyword ));
+		}( keyword ) );
 	}
 
 	return keyword;
 };
 
 /**
- * Returns an object containing as AST representation of the input (as `ast`) and any matching
- * comments discovered during parsing (as `comments`)
+ * Returns an object containing as AST representation of the input (as `ast`)
+ * and any matching comments discovered during parsing (as `comments`)
  *
  * @private
- * @return {Array} An object containing as AST representation of the input (as `ast`) and any
- * matching comments discovered during parsing (as `comments`)
+ * @param  {String} input String from which to find matches
+ * @return {Array}        An object containing as AST representation of the
+ *                        input (as `ast`) and any matching comments discovered
+ *                        during parsing (as `comments`)
  */
 XGettext.prototype._parseInput = function( input ) {
 	var comments = [],
@@ -144,13 +162,14 @@ XGettext.prototype._parseInput = function( input ) {
 			var isTranslatorComment = rxCommentMatch.test( text );
 
 			if ( isTranslatorComment ) {
-				comments.push({
+				comments.push( {
 					value: text.replace( rxCommentMatch, '' ).trim(),
 					line: comment.loc.start.line
-				});
+				} );
 			}
 		} );
 	}
+
 	return {
 		comments: comments,
 		ast: ast.program
@@ -158,19 +177,23 @@ XGettext.prototype._parseInput = function( input ) {
 };
 
 /**
- * Returns an array of objects representing all matched keywords, including the matched keyword
- * (as `keyword`), the CallExpression arguments array (as `arguments`), and potentially any comment
- * associated with the match (as `comment`)
+ * Returns an array of objects representing all matched keywords, including the
+ * matched keyword (as `keyword`), the CallExpression arguments array (as
+ * `arguments`), and potentially any comment associated with the match (as
+ * `comment`)
  *
  * @private
- * @return {Array} An array of objects representing all matched keywords
+ *
+ * @param  {Object} parsedInput Parse results
+ * @return {Array}              An array of objects representing all matched
+ *                              keywords
  */
 XGettext.prototype._discoverMatches = function( parsedInput ) {
 	var keywordFunctions = this.options.keywordFunctions,
 		matches = [];
 
 	walk( parsedInput.ast, {
-		enter: function( node, parent ) {
+		enter: function( node ) {
 			if ( node.type !== 'CallExpression' ) {
 				return;
 			}
@@ -183,10 +206,14 @@ XGettext.prototype._discoverMatches = function( parsedInput ) {
 			var functionName = ( callee.property ) ? callee.property.name : callee.name;
 
 			// Validate is named function
-			if ( ! functionName ) return;
+			if ( ! functionName ) {
+				return;
+			}
 
 			// Validate desired function name
-			if ( keywordFunctions.indexOf( functionName ) === -1 ) return;
+			if ( keywordFunctions.indexOf( functionName ) === -1 ) {
+				return;
+			}
 
 			// Build discovered match
 			var match = {
@@ -202,28 +229,33 @@ XGettext.prototype._discoverMatches = function( parsedInput ) {
 					node.loc.start.line - 1 === translatorComment.line ) {
 					match.comment = translatorComment.value;
 				}
-			});
+			} );
 
 			matches.push( match );
 		}
-	});
+	} );
 
 	return matches;
 };
 
 /**
- * Returns an object representing a single transformed matched keyword, including the transformed
- * keyword string value (as `string`), and potentially any comment associated with the match (as
- * `comment`)
+ * Returns an object representing a single transformed matched keyword,
+ * including the transformed keyword string value (as `string`), and
+ * potentially any comment associated with the match (as `comment`)
  *
  * @private
- * @return {Object} An object representing a single transformed matched keyword
+ *
+ * @param  {Object} match Match object
+ * @return {Object}       An object representing a single transformed matched
+ *                        keyword
  */
 XGettext.prototype._transformMatch = function( match ) {
 	var strings = this.options.keywords[ match.keyword ]( match );
 
 	// If transformed result is object, immediately return
-	if ( _.isPlainObject( strings ) ) return strings;
+	if ( _.isPlainObject( strings ) ) {
+		return strings;
+	}
 
 	// Cast strings to single-element array to enable mapping
 	if ( ! ( strings instanceof Array ) ) {
@@ -234,7 +266,7 @@ XGettext.prototype._transformMatch = function( match ) {
 	strings = strings.filter( Boolean );
 
 	// Transform string back to object with comment
-	strings = _.map(strings, function( string ) {
+	strings = _.map( strings, function( string ) {
 		var transformed = { string: string, line: match.line, column: match.column };
 
 		if ( typeof match.comment !== 'undefined' ) {
@@ -242,7 +274,7 @@ XGettext.prototype._transformMatch = function( match ) {
 		}
 
 		return transformed;
-	});
+	} );
 
 	return strings;
 };
