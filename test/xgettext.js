dd4f05ae955ab1b3d95f86e37f40cbbcaa9531bc
Jon Surrell <jon.surrell@automattic.com>
2018-06-05 14:33:35 +0200

    Add JSX test

--- test/xgettext.js
+++ test/xgettext.js
@@ -101,3 +101,13 @@ it( 'should parse ecma6 by default', function() {
 
 	expect( matches ).to.deep.equal( [ { string: 'Hello World!', line: 1, column: 13 } ] );
 } );
+
+it( 'should handle jsx by default', function() {
+	var source = '<MyComponent translatedProp={ _( "Hello " ) }>{ _( "World!" ) }</MyComponent>',
+		matches = new XGettext().getMatches( source );
+
+	expect( matches ).to.deep.equal( [
+		{ string: 'Hello ', line: 1, column: 30 },
+		{ string: 'World!', line: 1, column: 48 },
+	] );
+} );
